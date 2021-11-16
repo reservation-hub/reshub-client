427476b91677cf1e471cbf01f6eb21f48986c2aa
Jang Yoonsung <upthe15752@gmail.com>
2021-11-16 13:26:46 +0900

    screen fix // 冗長化防止修正

--- app/src/pages/error/Error.tsx
+++ app/src/pages/error/Error.tsx
@@ -3,16 +3,18 @@ import MainTemplate from '@components/Template/MainTemplate'
 
 const Error = () => {
   return (
-    <MainTemplate classes='absolute'>
-      <div className='w-full top-[45%] absolute'>
-        <div className='gird text-center'>
-          <span className='text-[3.2rem]'>PAGE NOT FOUND</span>
-          <br />
-          <span className='text-[1.6rem] mt-5'>
-            申し訳ありません。お探しのページが見つかりませんでした。
-          </span>
+    <MainTemplate>
+      <section className='h-[84.3%]'>
+        <div className='w-full top-[45%] absolute'>
+          <div className='gird text-center'>
+            <span className='text-[3.2rem]'>PAGE NOT FOUND</span>
+            <br />
+            <span className='text-[1.6rem] mt-5'>
+              申し訳ありません。お探しのページが見つかりませんでした。
+            </span>
+          </div>
         </div>
-      </div>
+      </section>
     </MainTemplate>
   )
 }
