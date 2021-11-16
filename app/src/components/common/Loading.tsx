fd52a087fe8b013c780dd4f5d25f1d2b92e486a3
Jang Yoonsung <upthe15752@gmail.com>
2021-11-16 13:32:58 +0900

    loading component修正

--- app/src/components/common/Loading.tsx
+++ app/src/components/common/Loading.tsx
@@ -3,12 +3,11 @@ import '@styles/global.css'
 
 const Loading = () => {
   const spinner =
-    'w-[10rem] h-[10rem] animate-spin border-4 border-t-transparent border-primary rounded-full'
+    'w-[5rem] h-[5rem] animate-spin border-4 border-t-transparent border-primary rounded-full'
 
   return (
     <div className='text-[2.4rem] box-center'>
       <div className={spinner} />
-      <span>Loading...</span>
     </div>
   )
 }
--- app/src/pages/auth/Login.tsx
+++ app/src/pages/auth/Login.tsx
@@ -5,7 +5,7 @@ import Loading from '@/components/common/Loading'
 
 const Login = () => {
   return (
-    <MainTemplate classes='absolute'>
+    <MainTemplate>
       <Loading />
     </MainTemplate>
   )
