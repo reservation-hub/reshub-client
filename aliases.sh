1f7074ebd1b3ae9f22ee4dc1f0b61d8c52df63ce
Eugene Sinamban <eugene.sinamban@gmail.com>
2022-01-31 23:10:53 +0900

    add prod settings

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,11 @@
 alias client-init="git submodule update --init reshub-deploy"
 
-alias rh-cl="docker-compose up client"
+alias rh-cl="cl-build && docker-compose up client"
+alias cl-build="docker-compose build client"
 alias cl-down="docker-compose down"
 alias cl-bash="docker-compose exec client bash"
 alias cl-log="docker-compose logs -f --tail 100 client "
+
+alias cl-prd-build="docker-compose build prod"
+alias cl-prd="rm -rf app/build && cl-prd-build && docker-compose run prod"
+alias cl-prd-deploy="cl-prd && scp -r app/build/build/* it-project:reshub-prod/client"
